1ab79a5122c770c837295b4dc19b8f367bab23e6
murillo <zhackychannel@gmail.com>
2026-02-12 13:48:04 -0300

    fix not .zig-cache

--- services/core-go/.zig-cache/tmp/b17d2795750d4d58-cgo-gcc-input-2356522895.o.d
+++
@@ -1,9 +0,0 @@
-.zig-cache\tmp\b17d2795750d4d58-cgo-gcc-input-2356522895.o: \
-  C:/Users/muril/AppData/Local/Temp/cgo-gcc-input-2356522895.c \
-  C:/Zig/lib/include/stddef.h C:/Zig/lib/include/__stddef_header_macro.h \
-  C:/Zig/lib/include/__stddef_ptrdiff_t.h \
-  C:/Zig/lib/include/__stddef_size_t.h \
-  C:/Zig/lib/include/__stddef_wchar_t.h \
-  C:/Zig/lib/include/__stddef_null.h \
-  C:/Zig/lib/include/__stddef_max_align_t.h \
-  C:/Zig/lib/include/__stddef_offsetof.h
